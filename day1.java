df820728f81bdc0f5a0dd0494a0a63d918731261
Vignesh-tech360 <80673377+Vignesh-tech360@users.noreply.github.com>
2022-09-29 23:02:12 +0530

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class day1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String letter = sc.nextLine();
+        System.out.println("Hello, World.");
+        System.out.println(letter);
+    }
+}
